1694cb59c5555ccf5624f4351e28c6d06a6256c1
Georgiab90 <67228479+Georgiab90@users.noreply.github.com>
2020-06-21 15:41:27 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+int score{};
+
+    cout << "enter your score (0-100)" << endl;
+    cin >> score;
+    
+if (score >1) {
+    cout << "Congrats" << endl;
+
+}
+    
+    
+
+
+
+//double calc_area_circle(double radius);
+//double calc_volume_cylinder(double radius, double height);
+//void area_circle();
+//void volume_cylinder();
+//
+//const double pi {3.14159};
+//
+//int main() {
+//    
+//    area_circle();
+//    volume_cylinder();
+//    return 0;
+//}
+//
+//double calc_area_circle(double radius) {
+//    //return pi * radius * radius;
+//return calc_area_circle(radius) * height;
+//}
+// void area_circle() {
+//     double radius{};
+//     cout << "\nEnter the radius of the circle";
+//     cin >> radius;
+//     
+//     cout << "The area of a circle with radius " << radius << " Is : " << calc_area_circle(radius) << endl;
+//     
+// }
+// 
+// void volume_cylinder() {
+//     double radius{};
+//     double height{};
+//     
+//     cout << "Enter radius of cylinder ";
+//     cin >> radius;
+//     cout << "Enter the height ";
+//     cin >> height;
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//const double pi {3.14159};
+//double calc_area_circle(double radius) {
+//    return pi * radius * radius;
+//}
+//double calc_volume_cylinder(double radius, double height) {
+//    //return pi * radius * radius * height;
+//    return calc_area_circle(radius) * height;
+//}
+//
+//void area_circle() {
+//    double radius{};
+//    cout << "\n Enter the radius of the circle : ";
+//    cin >> radius;
+//    
+//cout << "The area of a circle with radius" << radius << " is " << calc_area_circle(radius)  << endl;
+//}
+//
+//void volume_cylinder() {
+//    double radius{};
+//    double height {};
+//    cout << " Enter the radius of cylinder ";
+//    cin >> radius;
+//    cout << "Enter the height of cylidner";
+//    cin >> height;
+//    cout << "Teh volume of a cylinder with radius " << radius << "And height" << height << "is" << calc_volume_cylinder(radius, height) << endl;
+//    
+//}
+//
+//int main() {
+//    
+//area_circle();
+//volume_cylinder();
+//    
+    return 0;
+}
